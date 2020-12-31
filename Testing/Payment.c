7d349cab258cc2e57ced4b210ba39d30d1ded7f8
nawfal4756 <nawfalmehboob@gmail.com>
2020-12-31 20:48:46 +0500

    Payments Updated, CreditCardVerification Error Code Updated, and working on Print bill Function

--- Testing/Payment.c
+++ Testing/Payment.c
@@ -29,18 +29,46 @@ int Payment(float amount) {
     printf("Enter Card Number: ");
     scanf("%llu", data.cardNumber);
     fflush(stdin);
+    while (data.cardNumber < 1000000000000000 || data.cardNumber > 9999999999999999) {
+        printf("Incorrect Card Number\n");
+        printf("Enter Card Number Again: ");
+        scanf("%llu", data.cardNumber);
+        fflush(stdin);
+    }
 
     printf("Enter Card Holder Name: ");
     gets(data.name);
+    fflush(stdin);
 
     printf("Enter Card Expiry Month (mm): ");
     scanf("%d", data.expiryDate.month);
+    fflush(stdin);
+    while (data.expiryDate.month < 1 || data.expiryDate.month > 12) {
+        printf("Incorrect Month Entered!\n");
+        printf("Enter Card Expiry Month Again (mm): ");
+        scanf("%d", data.expiryDate.month);
+        fflush(stdin);
+    }
 
     printf("Enter Card Expiry Year (yyyy): ");
     scanf("%d", data.expiryDate.year);
+    fflush(stdin);
+    while (data.expiryDate.year < 1000 || data.expiryDate.year > 9999) {
+        printf("Incorrect Year Entered!\n");
+        printf("Enter Card Expiry Year Again (yyyy): ");
+        scanf("%d", data.expiryDate.year);
+        fflush(stdin);
+    }
 
     printf("Enter Card CVC: ");
     scanf("%d", data.cvc);
+    fflush(stdin);
+    while (data.cvc < 100 || data.cvc > 999) {
+        printf("Incorrect CVC Entered!\n");
+        printf("Enter Card CVC: ");
+        scanf("%d", data.cvc);
+        fflush(stdin);
+    } 
 
     length = strlen(data.name);
     for(counter1 = 0; counter1 < length; counter1++) {
