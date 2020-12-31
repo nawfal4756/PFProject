6cd51b213700cd39a4bba2a1750d83f5257972c0
nawfal4756 <nawfalmehboob@gmail.com>
2020-12-31 21:09:07 +0500

    Bat file removed

--- GitHubDeploy.bat
+++
@@ -1,4 +0,0 @@
-cd 'E:\@Fast Google Drive\Semester 1\Programming Fundamentals\Project\Project'
-git add .
-git commit -m "Payments Updated, CreditCardVerification Error Code Updated, and working on Print bill Function"
-git push origin master
