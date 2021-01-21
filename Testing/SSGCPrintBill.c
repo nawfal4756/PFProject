2b409d022de66fd77433dd51d473025886c6fdbd
fiza-arbani <fizamajeedadil@gmail.com>
2021-01-22 02:11:19 +0500

    Current month has been added

--- Testing/SSGCPrintBill.c
+++ Testing/SSGCPrintBill.c
@@ -24,7 +24,14 @@ int main() {
 }
 
 int SSGCPrintBill(struct SSGCData SSGC) {
-    int counter2, counter3;
+    int counter2,counter3,counter,maximum,index=0;
+    for(counter=0;counter<12;counter++) {
+        maximum=SSGC.billYear[0];
+        if(SSGC.billYear[counter]>=maximum) {
+            maximum=SSGC.billYear[counter];
+            index=counter+1;
+        }
+    }
     FILE* pointer;
     char consumerId[15], fileName[50] = "SSGCBill-";
 
@@ -41,6 +48,58 @@ int SSGCPrintBill(struct SSGCData SSGC) {
     fprintf(pointer, "-----------------------------------------------------------------------------------------------------------------\n");
     fprintf(pointer, "\t\t\t\t\tSui Southern Gas Company Limited\t\t\n");
     fprintf(pointer, "-----------------------------------------------------------------------------------------------------------------\n");
+
+    switch(index) {
+        case 1:
+        fprintf(pointer, "Current Month and Year: January,%d\n", maximum);
+        break;
+
+        case 2:
+        fprintf(pointer, "Current Month and Year: February,%d\n", maximum);
+        break;
+
+        case 3:
+        fprintf(pointer, "Current Month and Year: March,%d\n", maximum);
+        break;
+
+        case 4:
+        fprintf(pointer, "Current Month and Year: April,%d\n", maximum);
+        break;
+
+        case 5:
+        fprintf(pointer, "Current Month and Year: May,%d\n", maximum);
+        break;
+
+        case 6:
+        fprintf(pointer, "Current Month and Year: June,%d\n", maximum);
+        break;
+
+        case 7:
+        fprintf(pointer, "Current Month and Year: July,%d\n", maximum);
+        break;
+
+        case 8:
+        fprintf(pointer, "Current Month and Year: August,%d\n", maximum);
+        break;
+
+        case 9:
+        fprintf(pointer, "Current Month and Year: September,%d\n", maximum);
+        break;
+
+        case 10:
+        fprintf(pointer, "Current Month and Year: October,%d\n", maximum);
+        break;
+
+        case 11:
+        fprintf(pointer, "Current Month and Year: November,%d\n", maximum);
+        break;
+
+        case 12:
+        fprintf(pointer, "Current Month and Year: December,%d\n", maximum);
+        break;
+
+    }
+
     fprintf(pointer, "Consumer ID: %llu\n", SSGC.consumerId);
     fprintf(pointer, "Name: %s\n", SSGC.name);    
     fprintf(pointer, "Address: %s\n", SSGC.address);
